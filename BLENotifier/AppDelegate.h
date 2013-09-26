dda7b5ed12ff37edd58e6bec7f62960d8234a54e
Michal Bínovský <mbinovsky@sygic.com>
2013-09-26 21:47:52 +0200

    Initial Commit

---
+++ BLENotifier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLENotifier
+//
+//  Created by Michal Binovsky on 9/26/13.
+//  Copyright (c) 2013 Michal Binovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BLENotifier/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BLENotifier
+//
+//  Created by Michal Binovsky on 9/26/13.
+//  Copyright (c) 2013 Michal Binovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
